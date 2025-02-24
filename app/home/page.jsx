20d255b0dcdd59db29fe601b30c66a8abc39a6f0
Abraham Ebijuni <abreb1235@gmail.com>
2025-02-24 10:29:37 +0100

    Update page.jsx

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -23,12 +23,6 @@ export default function Home() {
   return (
     <>
       <MathJaxContext
-        config={{
-          tex: {
-            inlineMath: [["$", "$"]],
-            displayMath: [["$$", "$$"]],
-          },
-        }}
       >
         <Canvas
           res={res}
